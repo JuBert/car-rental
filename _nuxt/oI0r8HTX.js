caa715629086c525b7726fa46741d1fb815717f2
JuBert <julien.aubert.rbs@gmail.com>
2025-08-02 14:50:39 +0000

    Deploying to gh-pages from @ JuBert/car-rental@92f2a7f383ba437aeddd51f6a2f2b6f91efd42e8 🚀

---
+++ _nuxt/oI0r8HTX.js
@@ -0,0 +1,1 @@
+import{_ as t,x as o,z as r,y as n}from"./BXmW6MDe.js";const a={};function s(c,e){return n(),o("div",null,e[0]||(e[0]=[r("pre",null,"about",-1)]))}const u=t(a,[["render",s]]);export{u as default};
